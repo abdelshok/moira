48d7c36dd4c8f83c9f75a2523c3b5144cb649984
Abdel Shokair <abdelshokair@abdels-mbp.home>
2019-12-02 16:09:32 -0500

    Add error logging and return to initial menu in messagePrompt

--- inquirerLibrary/messagePrompt.js
+++ inquirerLibrary/messagePrompt.js
@@ -18,13 +18,14 @@ let inputHandlePrompt = (email) => {
         // I'll use email for now which prevents people faking their identity. Will figure out
         // how to implement nickname later
 
-        SB.connect(email, (user, error) => {
+        // Temporary new implementation connects the user to the sendbird API with the user name
+        SB.connect(nickname, (user, error) => {
             if (error) {
                 console.log('Error encountered while connecting to SB');
                 return;
             };
             // newFeature: Figure out how to add metaData like nickname, etc. later
-            SB.OpenChannel.getChannel('channel_one')
+            SB.OpenChannel.getChannel('channel_one') // ToChange: The channel here is hard-coded. That's not good. 
             .then((openChannel, error) => {
                 openChannel.enter((response, error) => {
                     if (error) {
@@ -34,6 +35,7 @@ let inputHandlePrompt = (email) => {
                     // Accepts an input message and makes sure to send it through the socket
                     // which is possible because we pass through the 'openChannel' object
                     console.log('Open channel reached');
+                    clear();
                     inputMessagePrompt(openChannel);
                 })
             })
@@ -51,13 +53,22 @@ let inputMessagePrompt = (openChannel) => {
         }
     ]
     inquirer.prompt(questions).then((answer) => {
-        let { message } = answer;
-        openChannel.sendUserMessage(message, (message, error) => {
-            if (error) {
-                console.log('Error', error);
+        try {
+            let { message } = answer;
+            if (message === 'exitexitexit') {
+                console.log(success('Messaging interface successfully exited.'))
+                messageOrConnectPrompt();
+            } else { // Allows the user to exit from the message Prompt and go back to Main menu
+                openChannel.sendUserMessage(message, (message, error) => {
+                    if (error) {
+                        console.log('Error', error);
+                    }
+                    inputMessagePrompt(openChannel);
+                })
             }
-            inputMessagePrompt(openChannel);
-        })
+        } catch (err) {
+            console.log(`Error reached is ${err}`);
+        }
     })
 }
 
@@ -67,5 +78,8 @@ module.exports = {
 }
 
 // External Modules
+const clear = require('clear');
 const inquirer = require('inquirer');
 // Internal Modules
+const { messageOrConnectPrompt } = require('./messageOrConnectPrompt');
+const { success, error } = require('../chalkLibrary');
