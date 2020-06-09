666b253178357af1f8b3997c55eb81cd78f45b10
abdelshok <aas698@nyu.edu>
2020-06-09 18:07:11 -0400

    Update SB credentials w Test k3y & package version

--- configurations/sendbirdOpen.js
+++ configurations/sendbirdOpen.js
@@ -5,4 +5,5 @@ const SendBird = require('sendbird');
 // const openSB = new SendBird({appId: `${process.env.OPEN_SENDBIRD_APP_ID}`});
 
 const openSB = new SendBird({appId: '9E08E706-8CB1-4220-9E0C-13D931D8BA7F'});
+
 module.exports.openSB = openSB;
